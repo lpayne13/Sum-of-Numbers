65ba751fea4a09264ead7ebaf94a60893b3dd0ba
lpayne13 <144291432+lpayne13@users.noreply.github.com>
2023-10-08 08:36:07 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// function calculateSum(n) {
+//     let sum = 0;
+  
+//     // Using a for loop to iterate from 1 to n
+//     for (let i = 1; i <= n; i++) {
+//       sum += i; // Add the current number to the sum
+//     }
+  
+//     return sum;
+//   }
+  
+//   // Example usage
+//   const n = 5; // You can replace 5 with any positive integer
+//   const result = calculateSum(n);
+  
+//   console.log(`The sum of numbers from 1 to ${n} is: ${result}`);
+  
